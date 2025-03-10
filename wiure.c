8fa86632005f9079346390ffb233e88931ea19f8
pavithra708 <pavithrarevankar70@gmail.com>
2025-03-10 16:52:04 +0530

    Create wiure.c

---
+++ wiure.c
@@ -0,0 +1,10 @@
+ayrbc   jdfuDescription:
+Display the number of applicants for each internship.
+
+Tasks:
+
+Track the count of applicants for each internship.
+Update the count when a new application is submitted.
+Show the total number of applicants on the internship listing.
+Expected Outcome:
+Users and admins can see how many people have applied for each internship.
